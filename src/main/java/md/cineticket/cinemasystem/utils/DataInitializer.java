852c77ec33aefc2caf10df3eec2f11a2b24a1c37
Eugeniu <eugeniu1337@gmail.com>
2026-03-21 20:22:11 +0000

    fix admin role

--- src/main/java/md/cineticket/cinemasystem/utils/DataInitializer.java
+++ src/main/java/md/cineticket/cinemasystem/utils/DataInitializer.java
@@ -1,27 +1,31 @@
 package md.cineticket.cinemasystem.utils;
 
 import lombok.RequiredArgsConstructor;
-import md.cineticket.cinemasystem.dto.RegisterRequest;
-import md.cineticket.cinemasystem.service.UserService;
+import md.cineticket.cinemasystem.model.Role;
+import md.cineticket.cinemasystem.model.User;
+import md.cineticket.cinemasystem.repo.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class DataInitializer {
 
-    private final UserService userService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     CommandLineRunner initData() {
         return args -> {
-            RegisterRequest registerRequest = new RegisterRequest();
-            registerRequest.setEmail("admin@mail.com");
-            registerRequest.setUsername("admin");
-            registerRequest.setPassword("admin");
+            User user = new User();
+            user.setEmail("admin@mail.com");
+            user.setUsername("admin");
+            user.setPassword(passwordEncoder.encode("admin"));
+            user.setRole(Role.ADMIN);
 
-            userService.createUser(registerRequest);
+            userRepository.save(user);
         };
     }
 
